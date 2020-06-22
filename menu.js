8e7c54fd4e58d7108a12048efd2f895bc06befe3
Sylvy Anscombe <sylvy.anscombe@gmail.com>
2020-06-22 01:15:11 +0100

    auto commit

--- menu.js
+++ menu.js
@@ -17,9 +17,11 @@
     <li><a>&nbsp;</a></li>
     <li><a class="navresearchsub" href="./newdirections.html">New Directions</a></li>
     <li><a class="navresearchsub" href="http://www.icms.org.uk/permutationgroups.php">FPGMT@ICMS</a></li>
+    <!--
     <li><a class="navseminarsub" href="./uclan_seminar.html">UCLan seminar 2015--2018</a></li>
     <li><a class="navseminarsub" href="http://personalpages.manchester.ac.uk/staff/Marcus.Tressl/events/LYMoTS.php">LYMoTS</a></li>
     <li><a class="navseminarsub" href="./leeds_seminar.html">Leeds seminar 2013--2015</a></li>
+    -->
 </ul>
 
 <ul class="nav">
