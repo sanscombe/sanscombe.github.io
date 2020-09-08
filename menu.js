35338f67a1db82664a3893a51babadb63dc87d86
Sylvy Anscombe <sylvy.anscombe@gmail.com>
2020-09-08 13:33:12 +0100

    auto commit

--- menu.js
+++ menu.js
@@ -3,14 +3,8 @@
     <li><a class="navmain" href="./">home</a></li>
     <li><a class="navresearchmain" href="./research.html">research</a></li>
     <li><a class="navteachingmain" href="./teaching.html">teaching</a></li>
-<!--
-    <li><a class="navteachingsub" href="./teaching.html#farrago">farrago</a></li>
--->
     <li><a class="navsmorgasbordmain" href="./smorgasbord.html">sm&ouml;rg&aring;sbord</a></li>
     <li><a class="navblogmain" href="https://sylvyanscombe.wordpress.com/">blog</a></li>
-<!--
-    <li><a class="navmain" href="./office.hours.html">office hours</a></li>
--->
     <li><a class="navmain" href="./contact.html">contact</a></li>
 </ul>
 
