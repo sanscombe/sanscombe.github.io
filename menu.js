b55ed670cf4d477a4b8c79143853179f60bb4cc3
Sylvy Anscombe <sylvy.anscombe@gmail.com>
2021-07-22 14:30:02 +0200

    auto commit

--- menu.js
+++ menu.js
@@ -25,5 +25,6 @@
 <ul class="nav">
     <li><a>&nbsp;</a></li>
     <li><a>&nbsp;</a></li>
+    <li><a class="navresearchmain" href="https://msp.org/mt">Model Theory (journal)</a></li>
     <li><a class="navdebugmain" href="https://unmaskedscience.uclan.ac.uk/">Unmasked</a></li>
 </ul>
