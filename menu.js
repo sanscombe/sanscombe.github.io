ae0602a134760f600ea080d206c2b40f19805547
Sylvy Anscombe <sylvy.anscombe@gmail.com>
2021-12-27 02:45:37 +0100

    auto commit

--- menu.js
+++ menu.js
@@ -20,7 +20,7 @@
     <li><a class="navseminarsub" href="http://personalpages.manchester.ac.uk/staff/Marcus.Tressl/events/LYMoTS.php">LYMoTS</a></li>
     <li><a class="navseminarsub" href="./leeds_seminar.html">Leeds seminar 2013--2015</a></li>
 	-->
-    <li><a class="navseminarsub" href="https://www.imj-prg.fr/gestion/evenement/affEvenement/66">S&eacute;eminaire G&eacute;n&eacute;ral</a></li>
+    <li><a class="navseminarsub" href="https://www.imj-prg.fr/gestion/evenement/affEvenement/66">S&eacute;minaire G&eacute;n&eacute;ral</a></li>
 </ul>
 
 <ul class="nav">
